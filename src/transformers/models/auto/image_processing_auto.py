b0abd3b1ba59648a3f246a1e242d563b9f5f750c
ydshieh <ydshieh@users.noreply.github.com>
2026-03-04 17:12:50 +0100

    vivit

--- src/transformers/models/auto/image_processing_auto.py
+++ src/transformers/models/auto/image_processing_auto.py
@@ -229,6 +229,7 @@ else:
             ("vit_msn", ("ViTImageProcessor", "ViTImageProcessorFast")),
             ("vitmatte", ("VitMatteImageProcessor", "VitMatteImageProcessorFast")),
             ("vitpose", ("VitPoseImageProcessor", "VitPoseImageProcessorFast")),
+            ("vivit", ("VivitImageProcessor", None)),
             ("xclip", ("CLIPImageProcessor", "CLIPImageProcessorFast")),
             ("yolos", ("YolosImageProcessor", "YolosImageProcessorFast")),
             ("zoedepth", ("ZoeDepthImageProcessor", "ZoeDepthImageProcessorFast")),
